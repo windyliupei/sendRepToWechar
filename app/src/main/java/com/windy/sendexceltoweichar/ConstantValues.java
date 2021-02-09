f5ff0bf2347eb598b3e2607242c5e98bfad5f516
windy <windy.liu@honeywell.com>
2021-02-09 16:07:31 +0800

    added files

---
+++ app/src/main/java/com/windy/sendexceltoweichar/ConstantValues.java
@@ -0,0 +1,12 @@
+package com.windy.sendexceltoweichar;
+
+public class ConstantValues {
+
+    public static String FILENAME_PREFIX = "天津君通津卫酒店疫情防控日报表-京津宾馆财务部部-";
+    public static String SHANGBAN = "上班";
+    public static String XIUXI = "休息";
+
+    public static int MAX_TEMP = 36;
+    public static int MIN_TEMP = 35;
+
+}
